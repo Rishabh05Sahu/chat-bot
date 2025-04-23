ff93bb595ca9cf5347e3ff40527ce35139645ffd
Rishabh Sahu <rishabhsahu13873@gmail.com>
2025-04-23 22:27:33 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const process = require('process');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
